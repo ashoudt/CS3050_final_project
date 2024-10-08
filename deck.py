d433e91f96f3041b523f0c3fa206e7502ef3ed17
agilpatr <agilpatr@uvm.edu>
2024-10-08 16:45:00 -0400

    Added comments to layout deck.py

---
+++ deck.py
@@ -0,0 +1,24 @@
+# Define functions to maintain a deck of cards
+# Good reference: https://api.arcade.academy/en/latest/tutorials/card_game/index.html
+
+# ***Define our constants (size of assets/values)***
+
+# Suspect, Weapon, and Room card type values
+SUSPECT_CARD_VALUES = ["Mrs Peacock", "Mrs White", "Miss Scarlett", "Prof Plum", "Col Mustard", "Mr Green"]
+WEAPON_CARD_VALUES = ["Candlestick", "Wrench", "Rope", "Lead Pipe", "Knife", "Revolver"]
+ROOM_CARD_VALUES = ["Conservatory", "Dining Room", "Library", "Billiard Room", "Lounge", "Kitchen", "Ball Room",
+                    "Study", "Hall"]
+
+# ***Create a Card Class***
+# Needs to keep track of what type of card it is (suspect, weapon, or room)
+# and what value that card holds
+
+# Needs to hold image for the card that matches the value
+
+
+# ***Create a Deck Class***
+# Function to shuffle the deck
+
+# Function to add 3 cards [one of each type] to the envelope in the middle of the board
+
+# Function to divide the remainder of the deck evenly between players
